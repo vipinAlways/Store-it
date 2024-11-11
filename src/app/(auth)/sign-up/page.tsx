5c569c334c95c9ab437c21d2ac6742e37ee58cb0
Your New Name <vipinkumarti555@gmail.com>
2024-11-11 19:46:27 +0530

    added auth page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,11 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+   
+     <AuthForm type='sign-up'/>
+    
   )
 }
 
