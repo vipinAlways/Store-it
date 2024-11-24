56d9ee456fc0d3e6a4c36ff7b4afc4b914ba9ad3
Your New Name <vipinkumarti555@gmail.com>
2024-11-24 18:54:05 +0530

    yes sir

--- src/components/ActionDropDown.tsx
+++ src/components/ActionDropDown.tsx
@@ -1,6 +1,15 @@
 "use client";
 import React, { useState } from "react";
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -8,15 +17,31 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Dialog } from "./ui/dialog";
+
 import Image from "next/image";
 import { Models } from "node-appwrite";
 import { actionsDropdownItems } from "@/constants";
+import Link from "next/link";
+import { constructFileUrl } from "@/lib/utils";
 
 const ActionDropDown = ({ file }: { file: Models.Document }) => {
   const [ismodalOpen, setIsmodalOpen] = useState(false);
   const [isDropDownOpen, setISDropDownOpen] = useState(false);
   const [action, setaction] = useState<ActionType | null>();
+
+  const renderDialogContent = () => {
+    return (
+      <DialogContent className="shad-dialog-button">
+        <DialogHeader className="flex flex-col gap-3">
+          <DialogTitle className=" text-center to-light-100">Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    );
+  };
   return (
     <Dialog open={ismodalOpen} onOpenChange={setIsmodalOpen}>
       <DropdownMenu open={isDropDownOpen} onOpenChange={setISDropDownOpen}>
@@ -37,13 +62,48 @@ const ActionDropDown = ({ file }: { file: Models.Document }) => {
             <DropdownMenuItem
               key={actionItem.value}
               className="shad-dropdown-item"
-              onClick={() => setaction(actionItem)}
+              onClick={() => {
+                setaction(actionItem);
+
+                if (
+                  ["rename", "share", "delete", "details"].includes(
+                    actionItem.value
+                  )
+                ) {
+                  setIsmodalOpen(true);
+                }
+              }}
             >
-              {actionItem.label}
+              {actionItem.value === "download" ? (
+                <Link
+                  href={constructFileUrl(file.bucketFileId)}
+                  download={file.name}
+                  className="flex items-center gap-2"
+                >
+                  <Image
+                    src={actionItem?.icon}
+                    alt="logo"
+                    width={30}
+                    height={30}
+                  />
+                  {actionItem.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={actionItem?.icon}
+                    alt="logo"
+                    width={30}
+                    height={30}
+                  />
+                  {actionItem.label}
+                </div>
+              )}
             </DropdownMenuItem>
           ))}
         </DropdownMenuContent>
       </DropdownMenu>
+      {renderDialogContent()}
     </Dialog>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -177,7 +177,12 @@ export const getFileIcon = (
 // APPWRITE URL UTILS
 // Construct appwrite file URL - https://appwrite.io/docs/apis/rest#images
 export const constructFileUrl = (bucketFileId: string) => {
-  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+  if (bucketFileId) {
+    return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+  }
+  else{
+    return "nahi hain "
+  }
 };
 
 export const constructDownloadUrl = (bucketFileId: string) => {
